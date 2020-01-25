073fbc98f2705f61d8a8aa29b346e53a3b66ced6
申鸿毓 <shenhongyu@shenhongyudembp.dyndns.rice.edu>
2020-01-25 17:11:41 -0600

    string generator good

--- Producer/DataGeneratorDefs.cs
+++ Producer/DataGeneratorDefs.cs
@@ -1,5 +1,7 @@
 ﻿using NumSharp;
 using Newtonsoft.Json.Linq;
+using System.Text;
+using System;
 
 namespace Producer
 
@@ -149,8 +151,17 @@ namespace Producer
 
         public string Generate()
         {
-            string data = "Random data (hard-coded for now)";
-            return data;
+
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int j = 0; j < this.MaxLen; j++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
         }
 
         public JValue GenerateJsonData()
